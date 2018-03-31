b1853f6c49f84765832083b9568aa36bd2e31285
Dragovorn <me@dragovorn.com>
2018-03-31 19:22:12 -0400

    hackermaned some more of the UI together, technically it works as a clicker

--- main.js
+++ main.js
@@ -9,6 +9,16 @@ function get(string) {
     return document.getElementById(string);
 }
 
+function addEvent(element, name, execute) {
+    if (element.attachEvent) {
+        element.attachEvent("on" + name, function() {
+            execute.call(element);
+        });
+    } else if (element.addEventListener) {
+        element.addEventListener(name, execute, false);
+    }
+}
+
 var Game = {};
 
 Game.Launch = function() {
@@ -17,13 +27,39 @@ Game.Launch = function() {
     Game.gold = 0;
     Game.gps = 0;
     
-    Game.Init = function() {
-        get("version").innerHTML = "v" + Game.version + (Game.beta ? " <span style='color:#f00;'>BETA</span>" : "");
-        
+    Game.CalculateClick = function() {
+        return 1; // TODO
+    }
+    
+    Game.ClickCreep = function() {
+        Game.gold = Game.gold + Game.CalculateClick();
+    }
+    
+    Game.Update = function() {
         Game.gold = Game.gold + Game.gps;
+    }
+    
+    Game.Render = function() {
         get("gold").innerHTML = Game.gold + " Gold";
         get("gps").innerHTML = Game.gps + " Gold/s";
         document.title = Game.gold + " Gold - Dota 2 Clicker";
+    }
+    
+    Game.Tick = function() {
+        Game.Update();
+        Game.Render();
+    }
+    
+    Game.Init = function() {
+        get("version").innerHTML = "v" + Game.version + (Game.beta ? " <span style='color:#f00;'>BETA</span>" : "");
+        
+        Game.Tick();
+        
+        addEvent(get("bigCreep"), "click", Game.ClickCreep)
+        
+        window.setInterval(function() {
+            Game.Tick();
+        }, 1000 / 60);
     }
     
     Game.Init();
